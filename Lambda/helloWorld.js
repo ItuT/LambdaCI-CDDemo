80621a3826ca328e100ceb03e3e39ac70918bb98
Itumeleng Tlali <tlalii663@gmail.com>
2021-05-10 11:50:49 +0200

    demo may

--- Lambda/helloWorld.js
+++ Lambda/helloWorld.js
@@ -1,3 +1,3 @@
 exports.handler =  (event, context, callback) => {
-    return callback(null, { statusCode: 200, body: "hello world"});
+    return callback(null, { statusCode: 200, body: "hello demo May"});
 };
